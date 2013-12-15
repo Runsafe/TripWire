a8f28230df6682c65fb757570fa0541db440e18b
Morten Nilsen <morten@runsafe.no>
2013-12-15 13:09:40 +0100

    Declare features used

--- src/no/runsafe/tripwire/Plugin.java
+++ src/no/runsafe/tripwire/Plugin.java
@@ -1,6 +1,8 @@
 package no.runsafe.tripwire;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Database;
+import no.runsafe.framework.features.Events;
 import no.runsafe.tripwire.database.TripwireLogRepository;
 import no.runsafe.tripwire.wires.BlockBreakAir;
 import no.runsafe.tripwire.wires.DiggingStraightDown;
@@ -10,6 +12,11 @@ public class Plugin extends RunsafeConfigurablePlugin
 	@Override
 	protected void PluginSetup()
 	{
+		// Framework features
+		addComponent(Events.class);
+		addComponent(Database.class);
+
+		// Plugin components
 		addComponent(TripwireLogRepository.class);
 		addComponent(BlockBreakAir.class);
 		addComponent(DiggingStraightDown.class);
