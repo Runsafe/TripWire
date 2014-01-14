e9e943b94c82c42ad89482ecd5ff4d0a7c8f6971
Morten Nilsen <morten@runsafe.no>
2014-01-14 23:34:47 +0100

    Upstream API changes

--- src/no/runsafe/tripwire/database/TripwireLogRepository.java
+++ src/no/runsafe/tripwire/database/TripwireLogRepository.java
@@ -1,11 +1,9 @@
 package no.runsafe.tripwire.database;
 
 import no.runsafe.framework.api.database.IDatabase;
+import no.runsafe.framework.api.database.ISchemaUpdate;
 import no.runsafe.framework.api.database.Repository;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import no.runsafe.framework.api.database.SchemaUpdate;
 
 public class TripwireLogRepository extends Repository
 {
@@ -33,20 +31,20 @@ public class TripwireLogRepository extends Repository
 	}
 
 	@Override
-	public HashMap<Integer, List<String>> getSchemaUpdateQueries()
+	public ISchemaUpdate getSchemaUpdateQueries()
 	{
-		HashMap<Integer, List<String>> revisions = new HashMap<Integer, List<String>>();
-		List<String> sql = new ArrayList<String>();
-		sql.add("CREATE TABLE IF NOT EXISTS `tripwire_events` (" +
-			"  `id` bigint(20) NOT NULL AUTO_INCREMENT," +
-			"  `player` varchar(50) NOT NULL," +
-			"  `category` int(11) NOT NULL," +
-			"  `message` varchar(150) NOT NULL," +
-			"  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP," +
-			"  PRIMARY KEY (`id`)," +
-			"  KEY `player` (`player`)" +
-			")");
-		revisions.put(1, sql);
-		return revisions;
+		ISchemaUpdate update = new SchemaUpdate();
+		update.addQueries(
+			"CREATE TABLE IF NOT EXISTS `tripwire_events` (" +
+				"  `id` bigint(20) NOT NULL AUTO_INCREMENT," +
+				"  `player` varchar(50) NOT NULL," +
+				"  `category` int(11) NOT NULL," +
+				"  `message` varchar(150) NOT NULL," +
+				"  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+				"  PRIMARY KEY (`id`)," +
+				"  KEY `player` (`player`)" +
+				")"
+		);
+		return update;
 	}
 }
